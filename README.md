625178e101b31d87488e124c18064d34aab2da99
Benjamin Kampmann <ben@gnunicorn.org>
2018-02-13 21:52:51 +0100

    first iteration

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ A Discourse Plugin to enhance moderation and allow more and topic specific featu
 ## ToDo / Upcoming
 
  - [ ] global thresholds
-    - [ ] max topics per user per day
-    - [ ] max posts per user per day
-    - [ ] max post per topic per user per 
+    - [*] max topics per user per day
+    - [*] max posts per user per day
+    - [*] max post per topic per user per 
     - [ ] increase by trust-level thresholds
  - [ ] lock person out of topic
  - [ ] lock person out of a topic temporarily
--- plugin.rb
+++ plugin.rb
@@ -2,3 +2,54 @@
 # about: Enhance Moderation Features for Discourse
 # version: 0.0.1
 # authors: Benjamin Kampmann, Ronja
+
+
+
+enabled_site_setting :moderation_pp_enabled
+
+
+require_dependency 'guardian'
+require_dependency 'current_user'
+
+module ModerationPlusPlusGuardian
+
+  def mpp_has_reached_daily_max_posts?
+  	return true if current_user
+  		.posts
+  		.public_posts
+  		.created_since(Time.now - 24.hours)
+  		.count() >= SiteSetting.moderation_pp_daily_max_posts
+  end
+
+  def mpp_has_reached_daily_max_posts_on_topic?(topic)
+  	return true if current_user
+  		.posts
+  		.public_posts
+  		.created_since(Time.now - 24.hours)
+  		.where(:topic => topic)
+  		.count() >= SiteSetting.moderation_pp_daily_max_posts_per_topic
+
+  end
+
+  def moderation_pp_can_create_post(parent)
+  	# we only restrict more
+  	return true unless parent
+  	return true unless SiteSetting.moderation_pp_enabled
+
+	binding.pry
+	return false if mpp_has_reached_daily_max_posts?
+	return false if mpp_has_reached_daily_max_posts_on_topic?(parent) 
+
+  	true
+  end
+
+  def can_create_post?(parent)
+    # We override the original Guardian method to figure out our own
+    return false if !moderation_pp_can_create_post(parent) 
+    super(parent)
+  end
+end
+
+Guardian.send :include, ModerationPlusPlusGuardian
+
+puts "yeah"
